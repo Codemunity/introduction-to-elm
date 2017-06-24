c1803fec1b75fec12a1af9a33097d2681ae20e76
Miguel Lopez V <mlopezva@akamai.com>
2017-06-23 18:34:10 -0600

    Section 3: Building a Validated Form - Lesson 13: Introduction

--- Main.elm
+++ Main.elm
@@ -8,36 +8,3 @@ main =
     , update = update
     , view = view
     }
-
--- MODEL
-
-initialModel = Model "" True
-
-type alias Model =
-  { nameInput : String
-  , isHidden: Bool
-  }
-
-type Msg
-  = ToggleVisibility
-  | NewInput String
-
--- VIEW
-
-view : Model -> Html Msg
-view model =
-  div [ class "container" ]
-    [ input [ type_ "text", onInput NewInput, placeholder "Name" ] []
-    , button [ onClick ToggleVisibility ] [ text "Toggle Visibility" ]
-    , p [ hidden model.isHidden ] [ text ("Hello, " ++ model.nameInput) ]
-    ]
-
--- UPDATE
-
-update : Msg -> Model -> Model
-update msg model =
-  case msg of
-    ToggleVisibility ->
-      { model | isHidden = not model.isHidden }
-    NewInput name ->
-      { model | nameInput = name }
