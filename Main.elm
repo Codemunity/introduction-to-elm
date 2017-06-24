4156864718583962f766e4420478cd4a28ec2f4f
Miguel Lopez V <mlopezva@akamai.com>
2017-06-23 18:43:40 -0600

    Section 3: Building a Validated Form - Lesson 15: View

--- Main.elm
+++ Main.elm
@@ -25,3 +25,20 @@ type Msg
 
 initialModel =
     Model "" "" "" []
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ input [ type_ "text", onInput NewEmail, placeholder "Email" ]
+    , input [ type_ "password", onInput NewPassword, placeholder "Password" ]
+    , input [ type_ "password", onInput NewConfirmPassword, placeholder "Confirm Password" ]
+    , div [ hidden (List.isEmpty model.errorMessages) ] [ errorsView model.errorMessages ]
+    ]
+
+errorsView : List String -> Html Msg
+errorsView errors =
+  ul [ style [ ("color", "red") ] ] (List.map errorView errors)
+
+errorView : String -> Html Msg
+errorView error =
+  li [] [ text error ]
